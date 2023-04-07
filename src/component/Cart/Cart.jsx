5ade80bae80a92affd84d0646df5035addb8ff48
sohanur28 <gsrahman28@gmail.com>
2023-04-07 18:29:26 +0600

    Conditional-Rendering-CSSclass

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,18 +1,57 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart, handleRemoveFromCart}) => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please add some products</p>
+    }
+    else {
+        message = <div>
+            <h3>Boroloxxx</h3>
+            <p><small>Thanks for giving your money</small></p>
+        </div>
+    }
     return (
         <div>
-            <h2>Order Summary: {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'blue' : 'red'}>Order Summary: {cart.length}</h2>
+            <p className={`bold bordered ${cart.length === 3 ? 'yellow' : 'purple'}`}>somethimg</p>
             {
-                cart.map(tshirt => <p 
+                cart.length > 2
+                    ? <span className='purple'>Aro kino</span>
+                    : <span>fokira</span>}
+            {message}
+            {
+                cart.map(tshirt => <p
                     key={tshirt._id}
-                    >{tshirt.name} <button 
+                >{tshirt.name} <button
                     onClick={() => handleRemoveFromCart(tshirt._id)}
-                    >X</button></p>)
+                >X</button></p>)
+            }
+            {
+                cart.length === 2 && <p>Double bonanza!!!</p>
+            }
+            {
+                cart.length === 3 || <h3>Tinta to hoilo na!!</h3>
             }
         </div>
     );
 };
 
 export default Cart;
+
+/**
+ * CONDITIONAL RENDERING
+ * 1. use if or if else to set a variable that will contain an element, comonents
+ * 2. ternary: condition ? 'for true' : 'false'
+ * 3. Logical &&: (if the condition is true then the next thing will be displayed)
+ * 4. Logical ||: (if the condition is false then the next thing will be displayed)
+ * 
+ * */ 
+
+/**
+ * CONDITIONAL CSS CLASS
+ * 1. use ternary
+ * 2. ternary inside template string
+ * 
+ * */ 
